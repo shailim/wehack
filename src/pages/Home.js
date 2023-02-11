c01415b155770df47a0b09fb5f645a18c2805887
Hannah Eason <silentvalley.hannah@gmail.com>
2023-02-11 17:18:41 -0600

    imageurl array

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,31 @@
 import '../styles/Home.css';
 import CareerButton from '../components/CareerButton'
 
-let titles = ["Researcher", "Programmer", "Civil Engineer", "Astronaut"]
+let objects = [
+    {
+        title: "Researcher",
+        image: "/researcher.png"
+    },
+    {
+        title: "Programmer",
+        image: "/coder.png"
+    },
+    {
+        title: "Civil Engineer",
+        image: "/engineer.png"
+    },
+    {
+        title: "Astronaut",
+        image: "/astronaut.png"
+    }
+]
 function Home() {
     return (
         <div className='body'>
             <div>Home Page</div>
             <div className='jobGrid'>
-                {titles.map(title => (
-                    <CareerButton text={title} imageUrl={'/coder.png'}></CareerButton>
+                {objects.map(object => (
+                    <CareerButton title={object.title} imageUrl={object.image}></CareerButton>
                 ))} </div>
         </div>
     );
