5b48a117a5aa34315340f35cd80b08d2328e43f3
Shreya Manda <mshreddy12@gmail.com>
2023-02-11 16:59:29 -0600

    home page grid

--- src/components/CareerButton.js
+++ src/components/CareerButton.js
@@ -1,17 +1,16 @@
 import '../styles/CareerButton.css'
 
-function CareerButton({title, imageUrl})
-{
-   return(
-      <div onClick={() => {alert("Hello from here")}} className="card-container">
-         <div className="image-container">
-            <img src={imageUrl} alt='hi' />
-         </div>
-         <div className="card-title">
-            <h3>{title}</h3>
-         </div>
-      </div>
-   );
+function CareerButton({ title, imageUrl }) {
+    return (
+        <div onClick={() => { alert("Hello from here") }} className="card-container">
+            <div className="image-container">
+                <img src={imageUrl} alt='hi' />
+            </div>
+            <div className="card-title">
+                <h3>{title}</h3>
+            </div>
+        </div>
+    );
 }
 
 export default CareerButton;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
-import CareerButton from '../components/CareerButton';
 import '../styles/Home.css';
+import CareerButton from '../components/CareerButton'
 
+let titles = ["Researcher", "Programmer", "Civil Engineer", "Astronaut"]
 function Home() {
     return (
-        <div>
-            <CareerButton
-            title='Coder'
-            imageUrl='/coder.png'/>
+        <div className='body'>
+            <div>Home Page</div>
+            <div className='jobGrid'>
+                {titles.map(title => (
+                    <CareerButton text={title} imageUrl={'/coder.png'}></CareerButton>
+                ))} </div>
         </div>
     );
 }
